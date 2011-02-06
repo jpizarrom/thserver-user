cdeb37ecdb2a8cfd77771c935a7dfa2812e7812b
Juan Pizarro <jpizarro@doingit.cl>
2011-02-06 16:30:28 -0300

    add ws

---
+++ thserver-user/src/main/java/com/jpizarro/th/server/user/util/UserUtils.java
@@ -0,0 +1,29 @@
+package com.jpizarro.th.server.user.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.jpizarro.th.lib.user.entity.UserTO;
+import com.jpizarro.th.server.user.model.entity.User;
+
+public class UserUtils {
+
+	public static UserTO teamTOFromTeam(User u) {
+		UserTO to = new UserTO();
+		
+		to.setUserId( u.getUserId());
+		to.setUsername( u.getUsername());
+		to.setPassword( u.getPassword());
+		
+		
+		
+		to.setRole( u.getRole());
+		
+		to.setLatitude( u.getLatitude());
+		to.setLongitude( u.getLongitude());
+
+		to.setName( u.getName());
+		
+		return to;
+	}
+}
---
+++ thserver-user/src/main/java/com/jpizarro/th/server/user/view/web/ws/FindByIdWS.java
@@ -0,0 +1,39 @@
+package com.jpizarro.th.server.user.view.web.ws;
+
+import org.apache.wicket.PageParameters;
+import org.wicketstuff.annotation.mount.MountPath;
+import org.wicketstuff.annotation.strategy.MountMixedParam;
+
+import com.jpizarro.th.lib.user.entity.UserTO;
+import com.jpizarro.th.lib.user.util.xml.xstream.UserXStreamFactory;
+import com.jpizarro.th.server.generic.model.persistence.util.exceptions.InstanceNotFoundException;
+import com.jpizarro.th.server.generic.view.web.ws.util.GenericWS;
+import com.jpizarro.th.server.user.model.service.UserService;
+import com.jpizarro.th.server.user.view.web.application.WicketApplication;
+import com.thoughtworks.xstream.XStream;
+
+@MountPath(path = "/ws/findById")
+@MountMixedParam(parameterNames={"userId"})
+public class FindByIdWS extends GenericWS {
+
+	public FindByIdWS(PageParameters parameters) {
+		super(parameters, new UserXStreamFactory());
+		// TODO Auto-generated constructor stub
+	}
+
+	public void	doExecute(PageParameters parameters){
+		long teamId = parameters.getLong("userId");
+		
+		UserService service = WicketApplication.get().getUserService();
+		try {
+			UserTO teamTO = service.find(teamId);
+			
+			XStream xf = xStreamFactory.createXStream();
+			this.selement = xf.toXML(teamTO);
+//			this.element = TOToXMLConversor.toXML(teamTO);
+		} catch (InstanceNotFoundException e) {
+			System.out.println("IOIO : " + e.getMessage());
+		}
+	}
+
+}
